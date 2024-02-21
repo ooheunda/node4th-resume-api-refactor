558a0dbc6107e952737f631e6ef8718d79720233
ooheunda <ooheunda@gmail.com>
2024-02-21 10:26:27 +0900

    ♻️ router 정리

--- src/routes/resumes.router.js
+++ src/routes/resumes.router.js
@@ -1,160 +1,20 @@
 import express from "express";
 import { prisma } from "../utils/index.js";
+import { ResumesController } from "../controllers/resumes.controller.js";
+import { ResumesService } from "../services/resumes.service.js";
+import { ResumesRepository } from "../repositories/resumes.repository.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-// 이력서 목록 조회 API
-router.get("/resumes", async (req, res, next) => {
-  try {
-    const { orderKey, orderValue } = req.query;
-    const key = orderKey ? orderKey : "createdAt";
-    const value = orderValue ? orderValue : "desc";
+const resumesRepository = new ResumesRepository(prisma);
+const resumesService = new ResumesService(resumesRepository);
+const resumesController = new ResumesController(resumesService);
 
-    const resumes = await prisma.resumes.findMany({
-      select: {
-        resumeId: true,
-        title: true,
-        status: true,
-        createdAt: true,
-        updatedAt: true,
-        user: {
-          select: {
-            userInfo: {
-              select: {
-                name: true,
-              },
-            },
-          },
-        },
-      },
-      orderBy: {
-        [key]: value,
-      },
-    });
-
-    if (!resumes[0])
-      return res.status(404).json({ message: "이력서 데이터가 없습니다." });
-
-    return res.status(200).json({ data: resumes });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// 이력서 생성 API
-router.post("/resumes", authMiddleware, async (req, res, next) => {
-  try {
-    const { userId } = req.user;
-    const { title, content } = req.body;
-
-    const resume = await prisma.resumes.create({
-      data: {
-        userId: +userId,
-        title,
-        content,
-        status: "APPLY",
-      },
-    });
-
-    return res.status(201).json({ data: resume });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// 이력서 상세 조회 API
-router.get("/resumes/:resumeId", async (req, res, next) => {
-  const { resumeId } = req.params;
-  const resume = await prisma.resumes.findFirst({
-    where: { resumeId: +resumeId },
-    select: {
-      resumeId: true,
-      title: true,
-      content: true,
-      status: true,
-      createdAt: true,
-      updatedAt: true,
-      user: {
-        select: {
-          userInfo: {
-            select: {
-              name: true,
-              age: true,
-            },
-          },
-        },
-      },
-    },
-  });
-
-  if (!resume)
-    return res.status(404).json({ message: "이력서 데이터가 없습니다." });
-  return res.status(200).json({ data: resume });
-});
-
-// 이력서 수정 API
-router.put("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
-  try {
-    const { userId } = req.user;
-    const { resumeId } = req.params;
-    const { title, content, status } = req.body;
-    if (!title && !content && !status)
-      return res.status(400).json({ message: "수정할 값을 입력해주세요." });
-
-    const resume = await prisma.resumes.findFirst({
-      where: { resumeId: +resumeId },
-    });
-    if (!resume)
-      return res.status(404).json({ message: "이력서가 존재하지 않습니다." });
-    if (+userId !== resume.userId)
-      return res
-        .status(401)
-        .json({ message: "본인의 이력서만 수정할 수 있습니다." });
-
-    const updatedTitle = title ? title : resume.title;
-    const updatedContent = content ? content : resume.content;
-    const updatedStatus = status ? status.toUpperCase() : resume.status;
-
-    await prisma.resumes.update({
-      where: { resumeId: +resumeId },
-      data: {
-        title: updatedTitle,
-        content: updatedContent,
-        status: updatedStatus,
-      },
-    });
-
-    return res.status(201).json({ message: "수정이 완료되었습니다." });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// 이력서 삭제 API
-router.delete("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
-  try {
-    const { userId } = req.user;
-    const { resumeId } = req.params;
-
-    const resume = await prisma.resumes.findFirst({
-      where: { resumeId: +resumeId },
-    });
-    if (!resume)
-      return res.status(404).json({ message: "이력서가 존재하지 않습니다." });
-    if (+userId !== resume.userId)
-      return res
-        .status(401)
-        .json({ message: "본인의 이력서만 삭제할 수 있습니다." });
-
-    await prisma.resumes.delete({
-      where: { resumeId: +resumeId },
-    });
-
-    return res.status(201).json({ message: "삭제가 완료되었습니다." });
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", resumesController.findAllResumes);
+router.get("/:resumeId", resumesController.findResumeById);
+router.post("/", authMiddleware, resumesController.createResume);
+router.patch("/:resumeId", authMiddleware, resumesController.updateResume);
+router.delete("/:resumeId", authMiddleware, resumesController.deleteResume);
 
 export default router;
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,110 +1,18 @@
 import express from "express";
 import { prisma } from "../utils/index.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import UsersController from "../controllers/users.controller.js";
+import UsersService from "../services/users.service.js";
+import UsersRepository from "../repositories/users.repository.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
-
-dotenv.config();
 
 const router = express.Router();
 
-// 회원가입 API
-router.post("/sign-up", async (req, res, next) => {
-  try {
-    const { email, password, checkPassword, name, age, college, gender } =
-      req.body;
-    if (password !== checkPassword)
-      return res.status(403).json({ message: "비밀번호가 일치하지 않습니다." });
-    if (password.length < 6)
-      return res
-        .status(403)
-        .json({ message: "비밀번호는 6자 이상으로 만들어주세요." });
+const usersRepository = new UsersRepository(prisma);
+const usersService = new UsersService(usersRepository);
+const usersController = new UsersController(usersService);
 
-    const isExistEmail = await prisma.users.findUnique({
-      where: { email },
-    });
-    if (isExistEmail)
-      return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const upperGender = gender ? gender.toUpperCase() : gender;
-
-    const user = await prisma.users.create({
-      data: {
-        email: email,
-        password: hashedPassword,
-        kind: "USER",
-      },
-    });
-
-    const userInfo = await prisma.userInfos.create({
-      data: {
-        userId: user.userId,
-        name,
-        age,
-        college,
-        gender: upperGender,
-      },
-    });
-
-    return res.status(201).json({
-      message: "회원가입이 완료되었습니다.",
-      data: {
-        email: user.email,
-        detail: userInfo,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// 로그인 API
-router.post("/sign-in", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await prisma.users.findUnique({ where: { email } });
-    if (!user)
-      return res
-        .status(404)
-        .json({ message: "해당하는 유저가 존재하지 않습니다." });
-
-    if (!(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: "비밀번호가 올바르지 않습니다." });
-    }
-
-    const accessToken = jwt.sign(
-      { userId: user.userId },
-      process.env.ACCESS_TOKEN_SECRET_KEY,
-      { expiresIn: "12h" }
-    );
-
-    res.cookie("authorization", `Bearer ${accessToken}`);
-
-    return res.status(200).json({ message: "로그인 되었습니다." });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// 유저(본인) 정보 조회 API
-router.get("/users", authMiddleware, async (req, res, next) => {
-  const { userId } = req.user;
-
-  const userInfo = await prisma.users.findFirst({
-    where: { userId: +userId },
-    select: {
-      userId: true,
-      email: true,
-      createdAt: true,
-      updatedAt: true,
-      userInfo: true,
-    },
-  });
-
-  return res.status(200).json({ data: userInfo });
-});
+router.post("/sign-up", usersController.userSignUp);
+router.post("/sign-in", usersController.userSignIn);
+router.get("/", authMiddleware, usersController.findUserInfoById);
 
 export default router;
