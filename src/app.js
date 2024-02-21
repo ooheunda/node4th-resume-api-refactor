f72b667614de88e5b34845fe9dcbc0aa2044287c
ooheunda <ooheunda@gmail.com>
2024-02-22 06:20:38 +0900

    :wrench: dotenv

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import ErrorHandler from "./middlewares/error-handler.middleware.js";
 import Logger from "./middlewares/logger.middleware.js";
 import router from "./routes/index.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
